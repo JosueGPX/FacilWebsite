da164552809674ad9b7d617b97a7b31d211c0744
JosueGPX <116833520+JosueGPX@users.noreply.github.com>
2024-06-23 23:09:10 -0700

    add files via upload 23 jun

--- js/main.js
+++ js/main.js
@@ -64,3 +64,69 @@
     
 })(jQuery);
 
+var isAnimating = false;
+var isOpen = false;
+var button = document.querySelector(".sticky-menu-container .outer-button");
+var menu = document.querySelector(".sticky-menu-container .inner-menu");
+var closeIcon = document.querySelector(".sticky-menu-container .outer-button .close-icon");
+var arrowIcon = document.querySelector(".sticky-menu-container .outer-button .arrow-icon");
+var menuItems = document.querySelectorAll(".sticky-menu-container .inner-menu > .menu-list > .menu-item");
+
+var itemTexts = document.querySelectorAll(".sticky-menu-container .inner-menu > .menu-list > .menu-item > .item-text");
+
+button.addEventListener("click", function(){
+  if(isAnimating) return;
+  this.classList.add("clicked");
+  menu.classList.toggle("closed");
+  
+  if(isOpen){
+    closeIcon.classList.remove("show");
+    closeIcon.classList.add("hide");
+    arrowIcon.classList.remove("hide");
+    arrowIcon.classList.add("show");
+    menuItems.forEach(function(item){
+      console.log(item);
+       item.classList.add("text-hides");
+    });
+    itemTexts.forEach(function(text, index){
+        setTimeout(function(){
+          text.classList.remove("text-in");
+        });
+    });
+    isOpen = false;
+  }
+  else{
+    closeIcon.classList.remove("hide");
+    closeIcon.classList.add("show");
+    arrowIcon.classList.remove("show");
+    arrowIcon.classList.add("hide");
+    menuItems.forEach(function(item){
+      console.log(item);
+       item.classList.remove("text-hides");
+    });
+    itemTexts.forEach(function(text, index){
+        setTimeout(function(){
+          text.classList.add("text-in");
+        }, index*150);
+    });
+    isOpen = true;
+  }
+  
+});
+
+button.addEventListener("animationstart", function(event){
+  isAnimating = true;
+});
+
+button.addEventListener("animationend", function(event){
+  isAnimating = false;
+  this.classList.remove("clicked");
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// When the user clicks on the button, open the modal
+window.onload = function () {
+    modal.style.display = "block";
+ }
